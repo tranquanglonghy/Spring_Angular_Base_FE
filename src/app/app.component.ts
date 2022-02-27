582b1788d51b5eb1370971d6c61fdc9a4ba43c62
tranq <longtq@smartosc.com>
2022-02-28 03:36:01 +0700

    base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_01_start_project';
+  title = 'First Angular App';
 }
